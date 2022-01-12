cc2046024c012ea9ae3610939ab2ebd78c1198a9
MaxmudAxmedov <axmedovmaxmud559@gmail.com>
2022-01-12 21:25:46 +0500

    Add html and css change

---
+++ scss/bloks/_footer.scss.d.ts
@@ -0,0 +1,4 @@
+import { StyleProp, TextStyle } from 'react-native'
+
+export const footer: StyleProp<TextStyle>
+export const network: StyleProp<TextStyle>
---
+++ scss/bloks/_header.scss.d.ts
@@ -0,0 +1,4 @@
+import { StyleProp, TextStyle } from 'react-native'
+
+export const head: StyleProp<TextStyle>
+export const nav: StyleProp<TextStyle>
